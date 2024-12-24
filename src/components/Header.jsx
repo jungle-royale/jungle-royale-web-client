d115169a5aa329b3bde60a26d04dc849ae3c0f8c
genieljw <abcd42215@gmail.com>
2024-12-24 16:34:43 +0900

    feat: Header.css 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,22 +9,21 @@ const Header = () => {
   return (
     <header>
       <div className="logo">
-        <img src="/assets/header_logo.png" alt="Eternal Snowman" />
-        <h1>Eternal Snowman</h1>
+        <a href="/">
+          <img src="/assets/header_logo.png" alt="Eternal Snowman" />
+        </a>        
       </div>
       <nav>
         <a href="/">소식</a>
         <a href="/ranking">랭킹</a>
-        <a href="/login">로그아웃</a>
+        {isLogin ? (
+          <a onClick={handleLogout} className="logout-link">로그아웃</a>
+        ) : (
+          <a onClick={handleLogin} className="login-link">로그인</a>
+        )}
         <a href="/mypage">마이페이지</a>
       </nav>
-      <div className="user-controls">
-        {isLogin ? (
-          <button onClick={handleLogout}>로그아웃</button>
-        ) : (
-          <button onClick={handleLogin}>로그인</button>
-        )}
-      </div>
+      
     </header>
   );
 };
