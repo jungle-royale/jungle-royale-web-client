e5c59673aa01248c97c9e6c5423175bd7e444506
genieljw <abcd42215@gmail.com>
2024-12-24 21:10:17 +0900

    fix: 홈화면 body 레이아웃 확장

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
-
+import "./Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
-  return(
-    <div>
-      <Button text="Room List" onClick={() => navigate("/rooms")}/>
+  return (
+    <div className="app">
+      {/* 대문 사진 */}
+      <div className="main-bg"></div>
+      <div>
+        <Button text="Room List" onClick={() => navigate("/rooms")} />
+      </div>
     </div>
   );
-
 };
 
 export default Home;
