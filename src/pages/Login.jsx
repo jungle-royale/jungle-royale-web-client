64b8e6c15e406dfc4e34becda528e7196f9a3885
jjm159 <wjdwoaud15@gmail.com>
2024-12-31 18:50:47 +0900

    fix: BASE URL 배포 환경 설정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
 
   const Rest_api_key = 'e8304b2a6b5aeb5020ef6abeb405115b';
   //const redirect_uri = "http://localhost:5173/login"; // 리다이렉트 URL 설정
-  const redirect_uri = `${import.meta.env.VITE_API_URL}/login`;
+  const redirect_uri = `${import.meta.env.KAKAO_REDIRECT_URL}/login`;
 
   const navigate = useNavigate();
   const { setIsLogin, setUserRole } = useLoginContext();
