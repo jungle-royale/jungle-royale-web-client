6f9d8be61e6e5deea02b7112f34727c3e76fb4b3
genieljw <abcd42215@gmail.com>
2025-01-06 13:12:22 +0900

    fix: QR 크기 조정

--- src/utils/QRcode.jsx
+++ src/utils/QRcode.jsx
@@ -2,6 +2,12 @@ import { QRCodeCanvas } from 'qrcode.react';
 import PropTypes from 'prop-types';
 
 const QRcode = ({ qrdata }) => {
+
+  const canvas = document.querySelector("canvas");
+  canvas.style.width = "300px";
+  canvas.style.height = "300px";
+  canvas.style.margin = "0 auto";
+
   return (
     <>
       <QRCodeCanvas value={qrdata} />
