101370aae7f039bc741b50819625eb9d4046c77b
genieljw <abcd42215@gmail.com>
2025-01-14 22:31:19 +0900

    fix: 우클릭&드래그 방지 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import PostViewer from "./pages/posts/PostViewer";
 import PostCreator from "./pages/posts/PostCreator";
 import PostEditor from "./pages/posts/PostEditor";
 import RoomCreater from "./pages/rooms/RoomCreater";
-
+import { useEffect } from "react";
 
 // Initialize loglevel
 if (import.meta.env.VITE_NODE_ENV === 'production') {
@@ -31,6 +31,22 @@ if (import.meta.env.VITE_NODE_ENV === 'production') {
 log.info("Application started");
 
 function App() {
+  useEffect(() => {
+    const preventDragHandler = (event) => {
+      event.preventDefault();
+    };
+    const preventContextMenuHandler = (event) => {
+      event.preventDefault();
+    }
+
+    window.addEventListener("dragstart", preventDragHandler);
+    window.addEventListener("contextmenu", preventContextMenuHandler);
+
+    return () => {
+      window.removeEventListener("dragstart", preventDragHandler);
+      window.removeEventListener("contextmenu", preventContextMenuHandler);
+    }
+  }, []);
 
   return (
     <>
