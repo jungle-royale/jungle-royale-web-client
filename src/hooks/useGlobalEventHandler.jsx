194a6fac9429f4d6624c382a2251ef024d50dd77
genieljw <abcd42215@gmail.com>
2025-01-15 14:39:08 +0900

    fix: 입력 오류 버그 수정

--- src/hooks/useGlobalEventHandler.jsx
+++ src/hooks/useGlobalEventHandler.jsx
@@ -8,17 +8,17 @@ const useGlobalEventHandler = () => {
     const preventContextMenuHandler = (event) => {
       event.preventDefault();
     }
-    const preventSelectionHandler = (event) => {
-      event.preventDefault();
-    }
+    // const preventSelectionHandler = (event) => {
+    //   event.preventDefault();
+    // }
   
     window.addEventListener("dragstart", preventDragHandler); //드래그 방지
-    window.addEventListener("mousedown", preventSelectionHandler);  //드래그 방지
+    // window.addEventListener("mousedown", preventSelectionHandler);  //드래그 방지
     window.addEventListener("contextmenu", preventContextMenuHandler);  //우클릭 방지
   
     return () => {
       window.removeEventListener("dragstart", preventDragHandler);
-      window.removeEventListener("mousedown", preventSelectionHandler);
+      // window.removeEventListener("mousedown", preventSelectionHandler);
       window.removeEventListener("contextmenu", preventContextMenuHandler);
     }
   }, []);
